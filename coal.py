39f1f80f3005666b7c68929cb323e8cd19b771dd
jarvis21632 <jarvis21632@gmail.com>
2026-06-12 11:29:42 +0800

    Update coal.py

--- coal.py
+++ coal.py
@@ -12,12 +12,8 @@ with sync_playwright() as p:
         timeout=120000
     )
 
+    page.screenshot(path="page.png", full_page=True)
+
     print("TITLE =", page.title())
-
-    content = page.content()
-
-    print("低硫 =", content.find("低硫燃料油"))
-    print("0.5 =", content.find("0.5"))
-    print("KL =", content.find("KL"))
 
     browser.close()
