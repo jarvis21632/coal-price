5e7f48589bb78acdf68a363a26798623b42b9c3d
jarvis21632 <jarvis21632@gmail.com>
2026-06-12 11:14:48 +0800

    Update coal.py

--- coal.py
+++ coal.py
@@ -2,12 +2,23 @@ import requests
 
 url = "https://www.cpc.com.tw/cp.aspx?n=53"
 
-r = requests.get(url, timeout=60)
+r = requests.get(
+    url,
+    timeout=60,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    }
+)
 
 html = r.text
 
-print("table =", html.find("<table"))
-print("tbody =", html.find("<tbody"))
-print("低硫 =", html.find("低硫"))
-print("燃料油 =", html.find("燃料油"))
-print("KL =", html.find("KL"))
+idx = html.find("燃料油")
+
+print("position =", idx)
+
+if idx >= 0:
+    start = max(0, idx - 2000)
+    end = min(len(html), idx + 4000)
+    print(html[start:end])
+else:
+    print("找不到")
